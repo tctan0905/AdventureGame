4644c1b3b7d213114c91838ed57183b00091e219
trnhi1010 <57781716+trnhi1010@users.noreply.github.com>
2021-11-22 10:05:13 +0700

    [Nhi]
    Create and add moving script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    //Follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    private void Update()
+    {
+        //Follow player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+}
